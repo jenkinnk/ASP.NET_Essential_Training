9f9496a10253800205a5f0747fbc6b36f2c4967e
jenkinnk <jenkinnk@gmail.com>
2016-02-09 20:14:43 -0500

    C# Page Created
    A C# based aspx file created to demonstrated variable embedded in the
    HTML code.

---
+++ HelloWorld/HelloFromCSharp.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class HelloFromCSharp : System.Web.UI.Page
+{
+    protected String var1 = "Hello From C#";
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    
+}
